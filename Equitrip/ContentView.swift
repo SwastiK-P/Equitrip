7fa6e1676e01934bcc8a3a5a6322a84f24fd8d2c
SwastiK-P <swastikpatil000@gmail.com>
2026-08-27 13:44:51 +0530

    Initial Commit

---
+++ Equitrip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Equitrip
+//
+//  Created by Swastik Patil on 27/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Equitrip/EquitripApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquitripApp.swift
+//  Equitrip
+//
+//  Created by Swastik Patil on 27/08/26.
+//
+
+import SwiftUI
+
+@main
+struct EquitripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
